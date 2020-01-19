e6a2fabd280451f7b651a9818418524f2949dace
Niraj Gurung <57787864+vividly789@users.noreply.github.com>
2020-01-19 15:08:40 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@
 #This is how we do!
 #This is long code. Make it short.
 #okay my friend
+#checking for webhooks!
 n1=float(input("enter the first number:"))
 oper=input("Enter the operator:")
 n2=float(input("enter the second number:"))
